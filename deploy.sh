a9598a615011f946e1ae1c1ad8f8b97a4dc83489
Dmitri Shuralyov <shurcooL@gmail.com>
2015-01-24 13:32:09 -0800

    deploy.sh: Fix scp copy destinations.

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ ssh root@where-you-at.com "service whereyouat stop"
 
 echo "Uploading..."
 scp ./main root@where-you-at.com:/home/whereyouat/
-scp -r ./assets/ root@where-you-at.com:/home/whereyouat/assets/
-scp -r ./js/ root@where-you-at.com:/usr/local/gopath/src/github.com/gophergala/correct-horse-battery-staple/js/
-scp -r ./common/ root@where-you-at.com:/usr/local/gopath/src/github.com/gophergala/correct-horse-battery-staple/common/
-scp -r ./mapview/ root@where-you-at.com:/usr/local/gopath/src/github.com/gophergala/correct-horse-battery-staple/mapview/
+scp -r ./assets/ root@where-you-at.com:/home/whereyouat/
+scp -r ./js/ root@where-you-at.com:/usr/local/gopath/src/github.com/gophergala/correct-horse-battery-staple/
+scp -r ./common/ root@where-you-at.com:/usr/local/gopath/src/github.com/gophergala/correct-horse-battery-staple/
+scp -r ./mapview/ root@where-you-at.com:/usr/local/gopath/src/github.com/gophergala/correct-horse-battery-staple/
 ssh root@where-you-at.com "chown -R whereyouat:whereyouat /home/whereyouat/"
 
 echo "Starting service"
