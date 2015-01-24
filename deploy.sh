e73fd99fd6c93f910458ad48c649e36f1a7e67e8
Paul Bergeron <paul.d.bergeron@gmail.com>
2015-01-23 22:09:51 -0800

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Building..."
+GOOS=linux go build ./*.go
+
+echo "Stopping service"
+ssh root@where-you-at.com "service whereyouat stop"
+
+echo "Uploading..."
+scp main root@where-you-at.com:/home/whereyouat/
+scp -r assets/. root@where-you-at.com:/home/whereyouat/assets/
+ssh root@where-you-at.com "chown -R whereyouat:whereyouat /home/whereyouat/"
+
+echo "Starting service"
+ssh root@where-you-at.com "service whereyouat start"
+
