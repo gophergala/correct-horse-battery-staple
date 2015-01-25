3690e4f97a76e193b64ac04c00ff2f0f1542d4f6
Joseph Hager <ajhager@gmail.com>
2015-01-25 13:34:52 -0600

    Add accuracy circles

--- mapview/marker.go
+++ mapview/marker.go
@@ -4,12 +4,25 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+func accuracyColor(a float64) string {
+	switch true {
+	case a < 10:
+		return "#0055ff"
+	case a < 50:
+		return "#00ff55"
+	case a < 100:
+		return "#ff5500"
+	}
+	return "#ff0000"
+}
+
 type Marker struct {
 	js.Object
-	Message *Popup
-	Lat     float64
-	Lng     float64
-	mapView *MapView
+	Message  *Popup
+	Lat      float64
+	Lng      float64
+	Accuracy *Circle
+	mapView  *MapView
 }
 
 func NewMarker(lat, lng float64) *Marker {
@@ -37,6 +50,9 @@ func (m *Marker) AddToMap(mapView *MapView) {
 	if m.Message != nil {
 		mapView.Call("addLayer", m.Message)
 	}
+	if m.Accuracy != nil {
+		mapView.Call("addLayer", m.Accuracy)
+	}
 }
 
 func (m *Marker) SetMessage(message string) {
@@ -56,9 +72,38 @@ func (m *Marker) SetMessage(message string) {
 	}
 }
 
+func (m *Marker) SetAccuracy(a float64) {
+	if a != 0 {
+		if m.Accuracy == nil {
+			m.Accuracy = NewCircle(m.Lat, m.Lng, a)
+			if m.mapView != nil {
+				m.mapView.Call("addLayer", m.Accuracy)
+			}
+		}
+		m.Accuracy.SetRadius(a)
+		color := accuracyColor(a)
+		m.Accuracy.SetStyle(js.M{
+			"color":       color,
+			"opacity":     0.5,
+			"fillColor":   color,
+			"fillOpacity": 0.25,
+		})
+	} else {
+		if m.Accuracy != nil && m.mapView != nil {
+			m.mapView.Call("removeLayer", m.Accuracy)
+			m.Accuracy = nil
+		}
+	}
+}
+
 func (m *Marker) RemoveFromMap(mapView *MapView) {
 	if m.Message != nil {
 		mapView.Call("removeLayer", m.Message)
 	}
+
+	if m.Accuracy != nil {
+		mapView.Call("removeLayer", m.Accuracy)
+	}
+
 	mapView.Call("removeLayer", m)
 }
