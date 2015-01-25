eaf5ef1d023f7cef2b3f731f3d8f340694ce1817
Paul Bergeron <paul.d.bergeron@gmail.com>
2015-01-24 17:10:47 -0800

    Messages show up as you type, messages only show if you say something

--- mapview/marker.go
+++ mapview/marker.go
@@ -9,14 +9,16 @@ type Marker struct {
 	Message *Popup
 	Lat     float64
 	Lng     float64
+	mapView *MapView
 }
 
 func NewMarker(lat, lng float64) *Marker {
 	return &Marker{
 		Object:  L.Call("marker", NewLatLng(lat, lng)),
-		Message: NewPopup(lat, lng),
+		Message: nil,
 		Lat:     lat,
 		Lng:     lng,
+		mapView: nil,
 	}
 }
 
@@ -24,19 +26,35 @@ func (m *Marker) SetLatLng(lat, lng float64) {
 	m.Lat = lat
 	m.Lng = lng
 	m.Call("setLatLng", NewLatLng(lat, lng))
-	m.Message.Call("setLatLng", NewLatLng(lat, lng))
+	if m.Message != nil {
+		m.Message.Call("setLatLng", NewLatLng(lat, lng))
+	}
 }
 
 func (m *Marker) AddToMap(mapView *MapView) {
+	m.mapView = mapView
 	m.Call("addTo", mapView)
-	mapView.Call("addLayer", m.Message)
+	if m.Message != nil {
+		mapView.Call("addLayer", m.Message)
+	}
 }
 
 func (m *Marker) SetMessage(message string) {
-	m.Message.SetContent(message)
+	if message != "" {
+		if m.Message == nil {
+			m.Message = NewPopup(m.Lat, m.Lng)
+		}
+		m.Message.SetContent(message)
+	} else {
+		if m.Message != nil && m.mapView != nil {
+			m.mapView.Call("removeLayer", m.Message)
+		}
+	}
 }
 
 func (m *Marker) RemoveFromMap(mapView *MapView) {
-	mapView.Call("removeLayer", m.Message)
+	if m.Message != nil {
+		mapView.Call("removeLayer", m.Message)
+	}
 	mapView.Call("removeLayer", m)
 }
